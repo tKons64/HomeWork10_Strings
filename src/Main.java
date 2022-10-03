27f9e1384fbd8af8b07136be344b7cff5bc36e11
tKons64 <spb_unex@mail.ru>
2022-10-03 18:14:20 +0300

    Выполнены домашние задания №1-№3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,52 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
     }
+
+    public static void task1() {
+        System.out.println();
+        System.out.println("Задание №1");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+
+        String fullName;
+
+        fullName = lastName + " " + firstName + " " + middleName;
+
+        System.out.println("ФИО сотрудника — " + fullName);
+
+    }
+
+    public static void task2() {
+        System.out.println();
+        System.out.println("Задание №2");
+
+        String fullName = "Ivanov Ivan Ivanovich";
+        String preparedFullName;
+
+        preparedFullName = fullName.toUpperCase();
+
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + preparedFullName);
+
+    }
+
+    public static void task3() {
+        System.out.println();
+        System.out.println("Задание №2");
+
+        String fullName = "Иванов Семён Семёнович";
+        String preparedFullName;
+
+        preparedFullName = fullName.replace('ё', 'е');
+
+        System.out.println("Данные ФИО сотрудника — " + preparedFullName);
+
+    }
+
 }
